100d7ba3213cfe276814beb86887a0dd508790a5
Wildindfw <42122656+Wildindfw@users.noreply.github.com>
2018-11-22 20:54:10 -0600

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -9,17 +9,20 @@
  * @license     General Public Licence http://www.opensource-socialnetwork.org/licence
  * @link        http://www.opensource-socialnetwork.org/licence
 */
-
+));
 define('__altm__', ossn_route()->com . 'Discordchat/');
 
 function Discordchat_init() {  
+ossn_register_sections_menu("newsfeed", array(
+        "text" => "DiscordChat",
+        "url" => "https://titanembeds.com/embed/459837648618389525?defaultchannel=461786959627419648&fixedsidenav=true&sametarget=true&theme=MetroEdge",
+        "section" => "ChatRoom",
+        "icon" => true,
+));
+ossn_register_sections_menu("newsfeed", array(
+        "text" => "How to Verify",
+        "url" => "https://sites.google.com/s/1hxljiVWhT-4RAJ1v6Kd7QsG88kqEwlyy/p/112sih1USnuvfMj8U3gFh-2pukijWMtzR/edit",
+        "section" => "Chatroom",
+        "icon" => true,
 
-    ossn_register_page('Discordchat', 'Discordchat');
-    if(ossn_isLoggedin()) {
-        $icon = ossn_site_url('components/Discordchat/images/icon.png');
-        ossn_register_sections_menu('newsfeed', array('text' => Discord(' "https://titanembeds.com/embed/459837648618389525?defaultchannel=461786959627419648&fixedsidenav=true&sametarget=true&theme=MetroEdge"'), 'url' => ossn_site_url('severload'), 'icon' => $icon, 'section' => 'links'));
-	}
-}
-	
-//initilize serverload component
 ossn_register_callback('ossn', 'init', 'Discordchat_init');
