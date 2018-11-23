04b25155bcfa7e713ce5acc3310b47444736f622
Wildindfw <42122656+Wildindfw@users.noreply.github.com>
2018-11-22 20:33:23 -0600

    Add files via upload

---
+++ locale/ossn.en.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * wIldin
+ *
+ * @packagewIldIN
+ * @author    Insomnia
+ * @copyright 2018 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+$en = array(
+
+   'discordchat' => 'Discord Chatroom',
+
+   
+);
+ossn_register_languages('en', $en);
